2fe97ed2ba539e49a76337b00d6bbdcfe832501e
D. GRAYMAN <juan.gzz.salz@gmail.com>
2018-11-28 07:48:33 -0600

    se agregaron los paths a los recursos del cliente

--- src/client/client.js
+++ src/client/client.js
@@ -5,5 +5,12 @@ window.onload = () => {
 
     socket.on('connect', () => {
         console.log('You are in general room')
+        startGame()
     })
+
+    function startGame(){
+        console.log("Starting game...")
+        const game = new Phaser.Game(800, 600, Phaser.AUTO, '', {preload: preload, create: create, update: update})
+        
+    }
 }
--- src/server/web.js
+++ src/server/web.js
@@ -6,5 +6,6 @@ const port = 3100
 console.log("Starting web server")
 app.use(express.static('src/client'))
 app.use('/socket.io', express.static('node_modules/socket.io-client/dist'))
+app.use('/phaser-ce', express.static('node_modules/phaser-ce'))
 
 app.listen(port, () => console.log("Web server running at port " + port))
